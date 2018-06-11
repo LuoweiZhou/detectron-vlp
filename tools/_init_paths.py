d9d0b9068c96e343dc1e876108ae1793040d4fe2
Luowei Zhou <luoweizhou@devfair0202.h2.fair>
2018-06-11 15:43:03 -0600

    modify lib path

--- tools/_init_paths.py
+++ tools/_init_paths.py
@@ -6,7 +6,8 @@ def add_path(path):
         sys.path.insert(0, path)
 
 # Add lib to PYTHONPATH
-lib_path = '/private/home/xinleic/pyramid/lib'
+# lib_path = '/private/home/xinleic/pyramid/lib'
+lib_path = '/private/home/luoweizhou/subsystem/detectron-xinlei/lib'
 
 print 'current lib path: %s' % lib_path
 add_path(lib_path)
