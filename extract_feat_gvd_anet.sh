ef300fc4f98e694bca0adad9aa7d20ce59db4968
Luowei Zhou <luozhou@macula.eecs.umich.edu>
2020-04-18 21:39:02 -0400

    Minor updates

--- extract_feat_gvd_anet.sh
+++ extract_feat_gvd_anet.sh
@@ -6,5 +6,5 @@ python tools/extract_features_gvd_anet.py \
   --output-dir $DATA_ROOT/fc6_feat_100rois \
   --det-output-file $DATA_ROOT/anet_detection_vg_fc6_feat_100rois.h5 \
   --max_bboxes 100 --min_bboxes 100 \
-  --list_of_ids $DATA_ROOT/split_ids_anet_entities.json $DATA_ROOT/frames_1_10frm \
+  --list_of_ids $DATA_ROOT/dic_anet.json $DATA_ROOT/frames_1_10frm \
   | tee log/log_extract_features_vg_100rois_gvd_anet
